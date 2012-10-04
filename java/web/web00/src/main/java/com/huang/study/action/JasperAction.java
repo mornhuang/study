0e52e0eadf7f2683c2f58f43a1e54668b7ad37d0
Morn <Morn.Huang@gmail.com>
2012-10-04 15:14:10 +0800

    修改

--- java/web/web00/src/main/java/com/huang/study/action/JasperAction.java
+++ java/web/web00/src/main/java/com/huang/study/action/JasperAction.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.ServletContext;
 
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 
 import org.apache.struts2.util.ServletContextAware;
 
@@ -19,35 +20,52 @@ import com.opensymphony.xwork2.ActionSupport;
 public class JasperAction extends ActionSupport implements ServletContextAware {
 	private static final long serialVersionUID = -2657479265527219661L;
 	private ServletContext context;
-	private List<Ireport> rptData = null;
-	private Map rptConf = new HashMap();
-	private Map htmlExpConf = new HashMap();
-
+	private List<Ireport> rptData;
+	private Map htmlRptConf;
+	private Map htmlExpConf;
+	private Map xlsRptConf;
+	private Map xlsExpConf;
+	
 	@Override
 	public String execute() throws Exception {
-		try {
-			IIreportDao dao = (IIreportDao) BeanContainer.getBean("jsprDao");
-			rptData = dao.getAll();
+		IIreportDao dao = (IIreportDao) BeanContainer.getBean("jsprDao");
+		rptData = dao.getAll();
 
-			rptConf.put("image1", new File(context.getRealPath("skin/en_US/img/icon_export.gif")));
-			rptConf.put("isShowImage", Boolean.TRUE);
-
-			htmlExpConf.put(JRHtmlExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
-			htmlExpConf.put(JRHtmlExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS,	Boolean.TRUE);
-			htmlExpConf.put(JRHtmlExporterParameter.IMAGES_DIR_NAME, context.getRealPath("skin/en_US/img"));
-			htmlExpConf.put(JRHtmlExporterParameter.IMAGES_URI, context.getContextPath() + "/skin/en_US/img/");
-		} catch (Exception e) {
-
-		}
+		htmlRptConf = new HashMap();
+		htmlRptConf.put("image1", new File(context.getRealPath("skin/en_US/img/icon_export.gif")));
+		htmlRptConf.put("isShowImage", Boolean.TRUE);
+		htmlRptConf.put("image1Link", context.getContextPath() + "/JsprXls.action");
+		
+		htmlExpConf = new HashMap();
+		htmlExpConf.put(JRHtmlExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+		htmlExpConf.put(JRHtmlExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS,	Boolean.TRUE);
+		htmlExpConf.put(JRHtmlExporterParameter.IMAGES_DIR_NAME, context.getRealPath("skin/en_US/img"));
+		htmlExpConf.put(JRHtmlExporterParameter.IMAGES_URI, context.getContextPath() + "/skin/en_US/img/");
+		
 		return SUCCESS;
 	}
 
+	public String exportXls() throws Exception {
+		IIreportDao dao = (IIreportDao) BeanContainer.getBean("jsprDao");
+		rptData = dao.getAll();
+
+		xlsRptConf = new HashMap();
+		xlsRptConf.put("image1", new File(context.getRealPath("skin/en_US/img/icon_export.gif")));
+		xlsRptConf.put("isShowImage", Boolean.FALSE);
+		
+		xlsExpConf = new HashMap();
+		xlsExpConf.put(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+		xlsExpConf.put(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
+		
+		return SUCCESS;
+	}
+	
 	public List<Ireport> getRptData() {
 		return rptData;
 	}
 
-	public Map getRptConf() {
-		return rptConf;
+	public Map getHtmlRptConf() {
+		return htmlRptConf;
 	}
 
 	public Map getHtmlExpConf() {
@@ -57,5 +75,13 @@ public class JasperAction extends ActionSupport implements ServletContextAware {
 	@Override
 	public void setServletContext(ServletContext context) {
 		this.context = context;
+	}
+
+	public Map getXlsRptConf() {
+		return xlsRptConf;
+	}
+
+	public Map getXlsExpConf() {
+		return xlsExpConf;
 	}
 }
