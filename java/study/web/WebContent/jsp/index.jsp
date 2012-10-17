8f679f9fb92e08b2f803158afc40a0e880cd46a6
Morn <Morn.Huang@gmail.com>
2012-10-18 00:28:55 +0800

    修改

---
+++ java/study/web/WebContent/jsp/index.jsp
@@ -0,0 +1,21 @@
+<%--
+file: index.jsp
+desc: Test Jsp
+--%>
+<html>
+    <head>
+        <title>
+            Hello World -- test J2EE
+        </title>
+    </head>
+    <body>
+        <%
+        for (int i = 1; i < 5; i++) {
+        %>
+        <h<%=i%>>Hello Word</h<%=i%>>
+        <%
+        }
+        %>
+    </body>
+</html>
+
---
+++ java/study/web/src/main/java/com/huang/web/model/IreportDao.java
@@ -0,0 +1,38 @@
+package com.huang.web.model;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+public class IreportDao extends HibernateDaoSupport implements IIreportDao {
+
+	@Override
+	public Ireport get(int id) {
+		return (Ireport)getHibernateTemplate().get(Ireport.class, id);
+	}
+
+	@Override
+	public void save(Ireport test) {
+		getHibernateTemplate().save(test);
+
+	}
+
+	@Override
+	public void update(Ireport test) {
+		getHibernateTemplate().update(test);
+
+	}
+
+	@Override
+	public void delete(Ireport test) {
+		getHibernateTemplate().delete(test);
+
+	}
+
+	@Override
+	public List<Ireport> getAll() {
+		return (List<Ireport>)getHibernateTemplate().find("from Ireport");
+	}
+
+}
