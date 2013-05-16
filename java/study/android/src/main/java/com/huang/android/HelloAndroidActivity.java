5fbb502a83b23502e99efaf8a8a5e558e13b3d24
Morn <Morn.Huang@gmail.com>
2013-05-16 17:31:49 +0800

    modify

--- java/study/android/src/main/java/com/huang/android/HelloAndroidActivity.java
+++ java/study/android/src/main/java/com/huang/android/HelloAndroidActivity.java
@@ -3,11 +3,13 @@ package com.huang.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
 
     private static String TAG = "android";
-
+    private TextView myTextView;
+    
     /**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after 
@@ -16,9 +18,17 @@ public class HelloAndroidActivity extends Activity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
+	
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // 引用颜色资源，设置背景色为红色
+//        getWindow().setBackgroundDrawableResource(R.color.red_bg);
+        
+        myTextView = (TextView)findViewById(R.id.textView2);
+        String str = getString(R.string.dynamic_txt).toString();
+        myTextView.setText(str);
         
         int i = R.layout.main;
         System.out.println(i);
