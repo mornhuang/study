c5a1798240b31d0e86a93adb48df451a45437f0c
Morn <Morn.Huang@gmail.com>
2013-06-02 13:05:02 +0800

    modify

---
+++ java/study/android-it/src/main/java/com/huang/study/android/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.huang.study.android.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.huang.study.android.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
