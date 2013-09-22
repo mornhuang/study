b3095025bf08bd80199f5ad7f685390b6081017a
Morn <Morn.Huang@gmail.com>
2013-09-22 12:50:06 +0800

    add

---
+++ java/web/src/main/java/com/huang/study/web/model/User.java
@@ -0,0 +1,35 @@
+package com.huang.study.web.model;
+
+/**
+ * Created By User : Morn
+ * Created DateTime: 13-9-21 下午10:49
+ * Descriptions    :
+ */
+public class User {
+    private String name;
+    private String pass;
+
+    public User() {
+    }
+
+    public User(String name, String pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
