f25bb68e07763901cd6319dbe099eec9d2544db0
Morn <Morn.Huang@gmail.com>
2015-05-13 15:11:41 +0800

    add python study files

---
+++ python/P2_django/books/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author     : Morn
+# DateTime   : 
+
+"""Simple Description
+
+Detailed description from here 
+"""
+
+if '__name__' == '__main__':
+    pass
